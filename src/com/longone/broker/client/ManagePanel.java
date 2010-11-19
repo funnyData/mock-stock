c0a2b9acb787f6c00b3ebc7cf93afd0235bb9127
fielding.wang <fielding.wang@6e8929bc-323a-42a3-bcbf-d5f5a98ecb3d>
2010-11-19 09:39:19 +0000

    UI and layout improvement

--- src/com/longone/broker/client/ManagePanel.java
+++ src/com/longone/broker/client/ManagePanel.java
@@ -3,29 +3,39 @@ package com.longone.broker.client;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
 
+import java.util.Date;
+
 public class ManagePanel extends VerticalPanel implements Initializable {
 
-    private static final String[] HEADERS = {"用户", "初始资金", "可用资金", "股票市值", "总市值", "盈亏总额", "盈亏比例", "股票持仓比例", " ", " "};
+    private static final String[] HEADERS = {"排名", "投资部门", "初始资金", "可用资金", "股票市值", "总市值", "盈亏总额", "盈亏比例", "股票持仓比例", " ", " "};
     private static final String[] TRANS_HISTORY_HEADERS = {"股票代码", "股票简称", "买卖方向", "成交价格", "数量", "佣金", "交易时间"};
     private static final String[] POSITION_HEADERS = {"代码", "名称", "数量", "成本价格", "产生佣金", "股票现价",
             "浮动盈亏", "浮动盈亏比例", "个股市值", "个股持仓比例"};
 
+    HorizontalPanel hp = new HorizontalPanel();
     private Button refreshBtn = new Button("刷新");
+    private Label lastModified = new Label();
+
     private StockServiceAsync stockSvc;
-    private Grid rankGrid = new Grid();
+    private Grid rankGrid = null;
     private Grid transGrid = null;
     private Grid positionGrid = null;
     private Label transUser = null;
 
     public ManagePanel(StockServiceAsync stockSvc) {
-        setSpacing(6);
+        lastModified.addStyleName("small-label");
+        hp.setWidth("100%");
+        rankGrid = new Grid(1, HEADERS.length);
+        setSpacing(4);
         this.stockSvc = stockSvc;
-        this.add(refreshBtn);
+        hp.add(refreshBtn);
+        this.add(hp);
         this.add(rankGrid);
         refreshBtn.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
@@ -34,11 +44,9 @@ public class ManagePanel extends VerticalPanel implements Initializable {
                 loadData();
             }
         });
-        rankGrid.resize(1, HEADERS.length);
         Util.createGridHeader(rankGrid, HEADERS);
     }
 
-    
 
     private void loadData() {
         // Set up the callback object.
@@ -56,6 +64,13 @@ public class ManagePanel extends VerticalPanel implements Initializable {
                 }
                 populateRankTable(info);
                 refreshBtn.setEnabled(true);
+
+                String time = "最后更新时间: " + DateTimeFormat.getFormat(DateTimeFormat.PredefinedFormat.DATE_SHORT).format(new Date()) +  " "
+                        + DateTimeFormat.getFormat(DateTimeFormat.PredefinedFormat.TIME_MEDIUM).format(new Date()); 
+                lastModified.setText(time);
+                hp.add(lastModified);
+                hp.setCellHorizontalAlignment(lastModified, ALIGN_RIGHT);
+                hp.setCellVerticalAlignment(lastModified, ALIGN_BOTTOM);
             }
         };
         // Make the call to the stock price service.
@@ -64,7 +79,7 @@ public class ManagePanel extends VerticalPanel implements Initializable {
 
     private void populateRankTable(AccountInfo[] info) {
         Util.removeGridData(rankGrid);
-        rankGrid.resizeRows(info.length+1);
+        rankGrid.resizeRows(info.length + 1);
         for (int row = 1; row <= info.length; row++) {
             insertRankTableRow(row, info[row - 1]);
         }
@@ -72,60 +87,63 @@ public class ManagePanel extends VerticalPanel implements Initializable {
 
     private void insertRankTableRow(int row, final AccountInfo info) {
         NumberFormat fmt = NumberFormat.getFormat("#,##0.00");
-        rankGrid.setWidget(row, 0, new Label(info.getDisplayName()));
-        rankGrid.getCellFormatter().addStyleName(row, 0, "textCell");            
-        rankGrid.setWidget(row, 1, new Label(fmt.format(info.getIntialPrincipal())));
-        rankGrid.getCellFormatter().addStyleName(row, 1, "numericCell");
-        rankGrid.setWidget(row, 2, new Label(fmt.format(info.getLeftCapitical())));
+        rankGrid.setWidget(row, 0, new Label(String.valueOf(row)));
+        rankGrid.getCellFormatter().addStyleName(row, 0, "textCell");
+        rankGrid.setWidget(row, 1, new Label(info.getDisplayName()));
+        rankGrid.getCellFormatter().addStyleName(row, 1, "textCell");
+        rankGrid.setWidget(row, 2, new Label(fmt.format(info.getIntialPrincipal())));
         rankGrid.getCellFormatter().addStyleName(row, 2, "numericCell");
-        rankGrid.setWidget(row, 3, new Label(fmt.format(info.getStockValue())));
+        rankGrid.setWidget(row, 3, new Label(fmt.format(info.getLeftCapitical())));
         rankGrid.getCellFormatter().addStyleName(row, 3, "numericCell");
-        rankGrid.setWidget(row, 4, new Label(fmt.format(info.getTotalValue())));
+        rankGrid.setWidget(row, 4, new Label(fmt.format(info.getStockValue())));
         rankGrid.getCellFormatter().addStyleName(row, 4, "numericCell");
-        rankGrid.setWidget(row, 5, new Label(fmt.format(info.getProfit())));
+        rankGrid.setWidget(row, 5, new Label(fmt.format(info.getTotalValue())));
         rankGrid.getCellFormatter().addStyleName(row, 5, "numericCell");
-        rankGrid.setWidget(row, 6, new Label(fmt.format(info.getProfitPct()) + "%"));
+        rankGrid.setWidget(row, 6, new Label(fmt.format(info.getProfit())));
         rankGrid.getCellFormatter().addStyleName(row, 6, "numericCell");
+        rankGrid.setWidget(row, 7, new Label(fmt.format(info.getProfitPct()) + "%"));
+        rankGrid.getCellFormatter().addStyleName(row, 7, "numericCell");
         if (info.getProfit() > 0) {
-
-            rankGrid.getCellFormatter().removeStyleName(row, 5, "negativeChange");
-            rankGrid.getCellFormatter().addStyleName(row, 5, "positiveChange");
+            rankGrid.getCellFormatter().removeStyleName(row, 7, "negativeChange");
+            rankGrid.getCellFormatter().addStyleName(row, 7, "positiveChange");
             rankGrid.getCellFormatter().removeStyleName(row, 6, "negativeChange");
             rankGrid.getCellFormatter().addStyleName(row, 6, "positiveChange");
         } else if (info.getProfit() < 0) {
-            rankGrid.getCellFormatter().removeStyleName(row, 5, "positiveChange");
-            rankGrid.getCellFormatter().addStyleName(row, 5, "negativeChange");
+            rankGrid.getCellFormatter().removeStyleName(row, 7, "positiveChange");
+            rankGrid.getCellFormatter().addStyleName(row, 7, "negativeChange");
             rankGrid.getCellFormatter().removeStyleName(row, 6, "positiveChange");
             rankGrid.getCellFormatter().addStyleName(row, 6, "negativeChange");
         } else {
-            rankGrid.getCellFormatter().removeStyleName(row, 5, "positiveChange");
-            rankGrid.getCellFormatter().removeStyleName(row, 5, "negativeChange");
+            rankGrid.getCellFormatter().removeStyleName(row, 7, "positiveChange");
+            rankGrid.getCellFormatter().removeStyleName(row, 7, "negativeChange");
             rankGrid.getCellFormatter().removeStyleName(row, 6, "positiveChange");
             rankGrid.getCellFormatter().removeStyleName(row, 6, "negativeChange");
         }
 
         if (info.getTotalValue() != 0) {
-            rankGrid.setWidget(row, 7, new Label(fmt.format(100.0 * info.getStockValue() / info.getTotalValue()) + "%"));
+            rankGrid.setWidget(row, 8, new Label(fmt.format(100.0 * info.getStockValue() / info.getTotalValue()) + "%"));
         } else {
-            rankGrid.setWidget(row, 7, new Label("--"));
+            rankGrid.setWidget(row, 8, new Label("--"));
         }
-        rankGrid.getCellFormatter().addStyleName(row, 7, "numericCell");
+        rankGrid.getCellFormatter().addStyleName(row, 8, "numericCell");
 
-        Button button = new Button("交易记录");
-        rankGrid.setWidget(row, 8, button);
-        button.addClickHandler(new ClickHandler() {
+        Anchor link = new Anchor("交易记录");
+        rankGrid.setWidget(row, 9, link);
+        link.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
                 showTransHistory(info.getUsername(), info.getDisplayName());
             }
         });
-
-        Button positionBtn = new Button("当前持仓");
-        rankGrid.setWidget(row, 9, positionBtn);
+        rankGrid.getCellFormatter().addStyleName(row, 9, "textCell");
+        
+        Anchor positionBtn = new Anchor("当前持仓");
+        rankGrid.setWidget(row, 10, positionBtn);
         positionBtn.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
                 showPosition(info.getUsername(), info.getDisplayName());
             }
         });
+        rankGrid.getCellFormatter().addStyleName(row, 10, "textCell");
     }
 
     private void showPosition(final String username, final String displayName) {
@@ -170,6 +188,7 @@ public class ManagePanel extends VerticalPanel implements Initializable {
                 // TODO: Do something with errors.
                 GWT.log(caught.toString());
             }
+
             public void onSuccess(DealLog[] logs) {
                 if (logs == null) {
                     Window.alert(MockStock.SESSION_TIMEOUT_MSG);
@@ -187,10 +206,10 @@ public class ManagePanel extends VerticalPanel implements Initializable {
     }
 
     private void createPositionTable(StockPosition[] positions) {
-        positionGrid = new Grid(positions.length+1, POSITION_HEADERS.length);
+        positionGrid = new Grid(positions.length + 1, POSITION_HEADERS.length);
         // Create table for stock Positions.
         Util.createGridHeader(positionGrid, POSITION_HEADERS);
-        positionGrid.setCellPadding(3);
+
 
         for (int i = 0; i < positions.length; i++) {
             Util.addPositionRow(positionGrid, positions[i], i + 1);
@@ -198,11 +217,11 @@ public class ManagePanel extends VerticalPanel implements Initializable {
     }
 
 
+
     private void createTransTable(DealLog[] logs) {
         transGrid = new Grid(logs.length + 1, TRANS_HISTORY_HEADERS.length);
 
-        Util.createGridHeader(transGrid, TRANS_HISTORY_HEADERS);
-        transGrid.setCellPadding(3);
+        Util.createGridHeader(transGrid, TRANS_HISTORY_HEADERS, "tableList2");
 
         NumberFormat fmt = NumberFormat.getFormat("#,##0.00");
         for (int row = 1; row <= logs.length; row++) {
@@ -227,6 +246,7 @@ public class ManagePanel extends VerticalPanel implements Initializable {
             transGrid.setWidget(row, 5, new Label(fmt.format(deal.getCommission())));
             transGrid.getCellFormatter().addStyleName(row, 5, "numericCell");
             transGrid.setWidget(row, 6, new Label(deal.getCreated()));
+            transGrid.getCellFormatter().addStyleName(row, 6, "textCell");
         }
     }
 
--- src/com/longone/broker/client/TransHistoryPanel.java
+++ src/com/longone/broker/client/TransHistoryPanel.java
@@ -17,7 +17,7 @@ public class TransHistoryPanel extends VerticalPanel implements Initializable{
 
     public TransHistoryPanel(StockServiceAsync stockSvc) {
         grid = new Grid(1, HEADERS.length);
-        Util.createGridHeader(grid, HEADERS);
+        Util.createGridHeader(grid, HEADERS, "tableList2");
         this.stockSvc = stockSvc;
         this.add(button);
         this.add(grid);
@@ -59,8 +59,11 @@ public class TransHistoryPanel extends VerticalPanel implements Initializable{
         for(int row=1; row<=logs.length;row++) {
             DealLog deal = logs[row-1];
             grid.setWidget(row, 0, new Label(deal.getCode()));
+            grid.getCellFormatter().addStyleName(row, 0, "textCell");
             grid.setWidget(row, 1, new Label(deal.getName()));
+            grid.getCellFormatter().removeStyleName(row, 1, "textCell");
             grid.setWidget(row, 2, new Label(deal.getBs()));
+            grid.getCellFormatter().removeStyleName(row, 2, "textCell");
             if("买入".equals(deal.getBs())) {
                 grid.getCellFormatter().removeStyleName(row, 2, "negativeChange");
                 grid.getCellFormatter().addStyleName(row, 2, "positiveChange");
@@ -76,9 +79,9 @@ public class TransHistoryPanel extends VerticalPanel implements Initializable{
             grid.setWidget(row, 5, new Label(fmt.format(deal.getCommission())));
             grid.getCellFormatter().addStyleName(row, 5, "numericCell");
             grid.setWidget(row, 6, new Label(deal.getCreated()));
+            grid.getCellFormatter().removeStyleName(row, 6, "textCell");
         }
     }
-
 
     public void initialize() {
         loadData();
